b1b7074f2c6fda9e2d5e5e2e58f4b610ba1c86b6
Raghav <bathla.raghav5@gmail.com>
2021-11-09 11:28:00 +0530

    [Raghav] Programming Constructs

---
+++ Dice.js
@@ -0,0 +1,2 @@
+let diceValue=Math.floor(Math.random()*10)%6+1;
+console.log("Dice Number: "+dicevalue);
---
+++ SumandAverageNumber.js
@@ -0,0 +1,8 @@
+let firstValue=Math.floor(Math.random()*20);
+let secondValue=Math.floor(Math.random()*20);
+let thirdValue=Math.floor(Math.random()*20);
+let fourthValue=Math.floor(Math.random()*20);
+let fifthValue=Math.floor(Math.random()*20);
+console.log("Sum of five values are : ",firstValue+secondValue+thirdValue+fourthValue+fifthValue);
+let avg = (firstValue+secondValue+thirdValue+fourthValue+fifthValue)/5;
+console.log("Average of five values ", avg);
